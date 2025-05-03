b87200d30668350c190392ac00bd2f8cd48e4240
Asya <119043723+anastansa@users.noreply.github.com>
2025-05-03 16:55:29 +0300

    added sound logic

--- src/types/metronome.ts
+++ src/types/metronome.ts
@@ -1,14 +1,15 @@
 export interface Config {
-  beatCount: number;
-  tempo: number;
+	beatCount: number;
+	tempo: number;
 }
 
 export interface Action {
-  type: 'setTempo' | 'setBeatCount';
-  data: Partial<Config>;
+	type: 'setTempo' | 'setBeatCount';
+	data: Partial<Config>;
 }
 
 export enum BeatType {
-  NORMAL,
-  STRONG,
+	WEAK,
+	NORMAL,
+	STRONG,
 }
