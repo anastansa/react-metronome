baf684da4877cc5180fa279e6d397a1d1fc85de8
Asya <119043723+anastansa@users.noreply.github.com>
2025-05-07 06:21:37 +0300

    sound works

--- src/components/Tempo.tsx
+++ src/components/Tempo.tsx
@@ -1,41 +1,40 @@
 import { ChangeEvent } from 'react';
-// import styles from './Tempo.module.css';
 import IncDec from './IncDec';
 import { MIN_TEMPO, MAX_TEMPO } from '../config/metronome-config';
 import './Tempo.css';
 
 interface Props {
-  tempo: number;
-  onTempoChanged: (tempo: number) => void;
+	tempo: number;
+	onTempoChanged: (tempo: number) => void;
 }
 
 export default function TempoSelector({ tempo, onTempoChanged }: Props) {
-  const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = +event.target.value;
+	const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+		const value = +event.target.value;
 
-    onTempoChanged(value);
-  };
+		onTempoChanged(value);
+	};
 
-  return (
-    <div className="">
-      <p>Tempo (BPM)</p>
+	return (
+		<div className="">
+			<p>Tempo (BPM)</p>
 
-      <div>
-        <IncDec
-          label={'' + tempo}
-          onDec={() => onTempoChanged(tempo - 1)}
-          onInc={() => onTempoChanged(tempo + 1)}
-        />
-      </div>
+			<div>
+				<IncDec
+					label={'' + tempo}
+					onDec={() => onTempoChanged(tempo - 1)}
+					onInc={() => onTempoChanged(tempo + 1)}
+				/>
+			</div>
 
-      <input
-        type="range"
-        min={MIN_TEMPO}
-        max={MAX_TEMPO}
-        step={5}
-        value={tempo}
-        onChange={changeHandler}
-      />
-    </div>
-  );
+			<input
+				type="range"
+				min={MIN_TEMPO}
+				max={MAX_TEMPO}
+				step={5}
+				value={tempo}
+				onChange={changeHandler}
+			/>
+		</div>
+	);
 }
--- src/services/recorded-click-service.ts
+++ src/services/recorded-click-service.ts
@@ -1,18 +1,21 @@
 import { BeatLevel } from '../types/metronome';
+import Click1 from '@/assets/sounds/click1.mp3';
+import Click2 from '@/assets/sounds/click2.mp3';
+import Click3 from '@/assets/sounds/click3.mp3';
 
 const audios = [
-  new Audio('click1.mp3'),
-  new Audio('click2.mp3'),
-  new Audio('click3.mp3'),
+	new Audio(Click1),
+	new Audio(Click2),
+	new Audio(Click3),
 ];
 
 export class RecordedClickService {
-  play(level: BeatLevel) {
-    const audio = audios[level];
+	play(level: BeatLevel) {
+		const audio = audios[level];
 
-    audio.play();
-    audio.currentTime = 0;
-  }
+		audio.play();
+		audio.currentTime = 0;
+	}
 }
 
 const recordedClickService = new RecordedClickService();
